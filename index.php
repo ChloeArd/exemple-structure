14becd142342005f4d4b51adfcdf3b92d71e01b5
ChloeArd <chloe.ardoise@gmail.com>
2021-02-01 17:37:21 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- Inclusion du header se trouvant dans le fichier _partials/header.php -->
+<?php
+$title = "Mon super index";
+include './_partials/header.php';
+include './_partials/menu.php';
+?>
+
+<!-- Page content -->
+<main>
+    <!-- Section 1 -->
+    <section>
+        Mon contenu en section 1
+    </section>
+
+    <!-- Section 2 -->
+    <section>
+        Mon contenu en section 2
+    </section>
+</main>
+
+<!-- faire un cipier coller et supprimer _partials .... -->
+<?php include './pages/_partials/footer.php';
